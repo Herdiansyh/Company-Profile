38752da41df26401e9dc037b3e4cb3e9a88bead6
Herdiansyh <mohammadherdiansyah84@gmail.com>
2025-08-18 12:13:33 +0700

    pembuatan model, dan route

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Appointment extends Model
 {
-    //
+    use HasFactory, SoftDeletes ;
+
+    protected $fillable = [
+        'phone_number',
+        'name',
+        'email',
+        'meeting_at',
+        'budget',
+        'brief',
+        'product_id',
+    ];
+    protected $casts = [
+        'meeting_at' => 'date',//untuk bisa menggunakan format tanggal 
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
 }
--- app/Models/CompanyKeypoint.php
+++ app/Models/CompanyKeypoint.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CompanyKeypoint extends Model
 {
     //
+      use HasFactory, SoftDeletes ;
+
+    protected $fillable = [
+        'company_about_id',
+        'keypoint',
+     
+    ];
 }
