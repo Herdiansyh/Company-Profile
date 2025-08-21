d6756f78d3fe61a84b51b4bb9eb8c1d8e943befc
Herdiansyh <mohammadherdiansyah84@gmail.com>
2025-08-21 11:13:39 +0700

    tampilkan data home page

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CompanyStatistic;
+use App\Models\HeroSection;
+use App\Models\OurPrinciple;
+use App\Models\OurTeam;
+use App\Models\Product;
+use App\Models\Testimonial;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
@@ -9,6 +15,13 @@ class FrontController extends Controller
     //
     public function index()
     {
-        return view('front.index');
+        $hero_sections = HeroSection::orderByDesc('id')->take(1)->get();
+        $testimonials = Testimonial::take(4)->get();
+        $statistics = CompanyStatistic::take(4)->get();
+        $principles = OurPrinciple::take(4)->get();
+        $products = Product::take(3)->get();
+        $teams = OurTeam::take(7)->get();
+
+        return view('front.index', compact('statistics', 'principles', 'products', 'teams', 'testimonials', 'hero_sections'));
     }
 }
